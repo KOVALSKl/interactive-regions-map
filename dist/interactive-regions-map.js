5cd60a2368fabc731ca554ae2730d3e71c42fdb2
KOVALSKl <mihaenko11@mail.ru>
2024-03-13 16:36:09 +0300

    fix: Rebuild project with changes

--- dist/interactive-regions-map.js
+++ dist/interactive-regions-map.js
@@ -1,16 +1,16 @@
-import { openBlock as v, createElementBlock as k, createElementVNode as I, resolveComponent as g, createBlock as b, withCtx as p, createVNode as m, createTextVNode as x, toDisplayString as z, ref as l, computed as V, onMounted as F, Fragment as J, renderList as L, renderSlot as H } from "vue";
+import { openBlock as v, createElementBlock as k, createElementVNode as I, resolveComponent as g, createBlock as b, withCtx as p, createVNode as m, createTextVNode as x, toDisplayString as C, ref as l, computed as F, onMounted as L, Fragment as O, renderList as q, renderSlot as H } from "vue";
 import * as r from "d3";
-const q = { class: "map-region" }, G = ["d"], K = {
+const G = { class: "map-region" }, J = ["d"], K = {
   __name: "MapRegion",
   props: ["d", "data", "onclick"],
   setup(a) {
     const e = a;
-    return (i, c) => (v(), k("g", q, [
+    return (i, c) => (v(), k("g", G, [
       I("path", {
         class: "map-region-path",
         d: e.d,
         onClick: c[0] || (c[0] = (o) => a.onclick(o, a.data))
-      }, null, 8, G)
+      }, null, 8, J)
     ]));
   }
 }, Z = {
@@ -27,13 +27,13 @@ const q = { class: "map-region" }, G = ["d"], K = {
         default: p(() => [
           m(c, { class: "region-title" }, {
             default: p(() => [
-              x(z(a.regionData.properties.NAME_1), 1)
+              x(C(a.regionData.properties.NAME_1), 1)
             ]),
             _: 1
           }),
           m(o, { class: "region-info" }, {
             default: p(() => [
-              x(z(a.regionData.properties), 1)
+              x(C(a.regionData.properties), 1)
             ]),
             _: 1
           }),
@@ -70,7 +70,7 @@ const q = { class: "map-region" }, G = ["d"], K = {
   fill: "none",
   stroke: "white",
   "stroke-linejoin": "round"
-}, null, -1), C = 1500, ee = {
+}, null, -1), N = 1500, ee = {
   __name: "RegionsMap",
   props: {
     width: {
@@ -83,15 +83,15 @@ const q = { class: "map-region" }, G = ["d"], K = {
     },
     regions: {
       type: Object,
-      default: regionsJSON
+      required: !0
     },
     regionsIndexes: {
       type: Object,
-      default: regionsIndexesJSON
+      default: {}
     }
   },
   setup(a) {
-    const e = a, i = e.regions.features.length, c = r.geoTransverseMercator().fitSize([e.width, e.height], e.regions), o = l(0), h = l(r.geoPath().projection(c)), _ = l(null), d = l(null), u = l(null), f = l(null), R = l(null), y = V(() => e.regions.features[o.value]);
+    const e = a, i = e.regions.features.length, c = r.geoTransverseMercator().fitSize([e.width, e.height], e.regions), o = l(0), h = l(r.geoPath().projection(c)), _ = l(null), d = l(null), u = l(null), f = l(null), R = l(null), y = F(() => e.regions.features[o.value]);
     function D(t) {
       t >= 0 && t < i && (o.value = t);
     }
@@ -114,28 +114,28 @@ const q = { class: "map-region" }, G = ["d"], K = {
       let t = y.value.properties.id;
       document.getElementById(t).lastChild.dispatchEvent(new MouseEvent("click", void 0));
     }
-    function S(t) {
+    function A(t) {
       const { transform: n } = t;
       f.value.attr("transform", n), f.value.attr("stroke-width", 1 / n.k);
     }
-    function O() {
-      u.value.transition().duration(C).call(
+    function S() {
+      u.value.transition().duration(N).call(
         d.value.transform,
         r.zoomIdentity,
         r.zoomTransform(u.value.node()).invert([e.width / 2, e.height / 2])
       );
     }
-    function A(t, n) {
-      const [[s, E], [N, $]] = h.value.bounds(n);
+    function V(t, n) {
+      const [[s, E], [$, z]] = h.value.bounds(n);
       t.stopPropagation(), R.value.transition().style("fill", null), r.select(t.currentTarget).transition().style("fill", "red");
       let w;
-      e.regionsIndexes ? w = B(n) : w = T(n), D(w), u.value.transition().duration(C).call(
+      e.regionsIndexes ? w = B(n) : w = T(n), D(w), u.value.transition().duration(N).call(
         d.value.transform,
-        r.zoomIdentity.translate(e.width / 2, e.height / 2).scale(Math.min(8, 0.9 / Math.max((N - s) / e.width, ($ - E) / e.height))).translate(-(s + N) / 2, -(E + $) / 2)
+        r.zoomIdentity.translate(e.width / 2, e.height / 2).scale(Math.min(8, 0.9 / Math.max(($ - s) / e.width, (z - E) / e.height))).translate(-(s + $) / 2, -(E + z) / 2)
       );
     }
-    return F(() => {
-      d.value = r.zoom().scaleExtent([1, 8]).on("zoom", S), u.value = r.select("svg").on("click", O).call(d.value), R.value = r.selectAll("path"), f.value = r.select("#regions-container");
+    return L(() => {
+      d.value = r.zoom().scaleExtent([1, 8]).on("zoom", A), u.value = r.select("svg").on("click", S).call(d.value), R.value = r.selectAll("path"), f.value = r.select("#regions-container");
     }), (t, n) => (v(), k("div", Q, [
       (v(), k("svg", {
         id: "map",
@@ -145,14 +145,14 @@ const q = { class: "map-region" }, G = ["d"], K = {
         viewBox: [0, 0, e.width, e.height]
       }, [
         I("g", W, [
-          (v(!0), k(J, null, L(a.regions.features, (s) => (v(), b(K, {
+          (v(!0), k(O, null, q(a.regions.features, (s) => (v(), b(K, {
             key: s.properties.NAME_1,
             id: s.properties.id,
             ref_for: !0,
             ref: "regionsRef",
             data: s,
             d: h.value(s),
-            onclick: A
+            onclick: V
           }, null, 8, ["id", "data", "d"]))), 128)),
           X
         ])
